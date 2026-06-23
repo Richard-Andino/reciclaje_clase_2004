0b1bb26991c1742e313ae88519ae921e3d24b657
Richard-Andino <99428432+Richard-Andino@users.noreply.github.com>
2026-06-23 10:27:45 -0600

    Initialize Streamlit app with imports and title
    Added imports and Streamlit configuration for the app.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import json
+from pathlib import Path
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
 
+st.set_page_config(page_tittle = "Reciclaje IA_ISC".layout = "centered")
+st.tittle("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026")
+st.write("Suba una imagen para clasificar con el modelo Mobilenet V2 pre entrenado")
