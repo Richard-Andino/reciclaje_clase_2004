7520b930026063750510943a1f5f0c1f80bf0113
Richard-Andino <99428432+Richard-Andino@users.noreply.github.com>
2026-06-23 10:33:23 -0600

    Fix typo in set_page_config title parameter

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 
-st.set_page_config(page_tittle = "Reciclaje IA_ISC", layout = "centered")
+st.set_page_config(page_title = "Reciclaje IA_ISC", layout = "centered")
 st.tittle("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026")
 st.write("Suba una imagen para clasificar con el modelo Mobilenet V2 pre entrenado")
